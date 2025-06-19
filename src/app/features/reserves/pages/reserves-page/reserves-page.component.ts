488b6d62179d7cb226413f042642d2889917cd88
Arnau Martinez Manchado <arnau.martinez@uvesolutions.com>
2025-06-19 11:58:37 +0200

    estructura básica de componentes + landing page

---
+++ src/app/features/reserves/pages/reserves-page/reserves-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pelu-reserves-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './reserves-page.component.html',
+  styleUrl: './reserves-page.component.scss'
+})
+export class ReservesPageComponent {
+
+}
