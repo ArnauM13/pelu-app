b8777991b5380766c379db583c378f260a18bad0
Arnau Martinez Manchado <arnaumm98@gmail.com>
2025-06-21 15:34:54 +0200

    landing page

---
+++ src/app/pages/landing/landing-page/landing-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'pelu-landing-page',
+  standalone: true,
+  imports: [CommonModule, RouterModule, ButtonModule, CardModule],
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent {
+  year = new Date().getFullYear();
+}
