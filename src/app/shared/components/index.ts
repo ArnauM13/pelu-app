280bd7a4789517b52beb26dc0331e54f6b0bdad9
Arnau Martinez Manchado <arnau.martinez@uvesolutions.com>
2025-06-25 13:29:17 +0200

    neteja comps

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,7 @@
+// Existing components
+export { FloatingButtonComponent } from './floating-button/floating-button.component';
+export type { FloatingButtonConfig } from './floating-button/floating-button.component';
+export { CardComponent } from './card/card.component';
+export { InfoItemComponent } from './info-item/info-item.component';
+export { PopupStackComponent } from './popup-stack/popup-stack.component';
+export type { PopupItem } from './popup-stack/popup-stack.component';
