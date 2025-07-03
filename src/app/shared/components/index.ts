36fd185734b75f255510cd75fcdf5924a87eeaf4
Arnau Martinez Manchado <arnaumm98@gmail.com>
2025-07-03 18:41:37 +0200

    booking popup

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -8,3 +8,5 @@ export type { PopupItem } from './popup-stack/popup-stack.component';
 export { LanguageSelectorComponent } from './language-selector/language-selector.component';
 export { FiltersPopupComponent } from './filters-popup/filters-popup.component';
 export { FiltersInlineComponent } from './filters-inline/filters-inline.component';
+export { BookingPopupComponent } from './booking-popup/booking-popup.component';
+export type { BookingDetails } from './booking-popup/booking-popup.component';
