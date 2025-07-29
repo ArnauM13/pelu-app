97e904bc8409bc054b1691755dc42ef8338419d3
arnaum13 <arnaumm98@gmail.com>
2025-07-29 23:10:20 +0200

    feat: update admin, appointments, auth and routing with improved functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { RegisterPageComponent } from './features/auth/register-page/register-pa
 import { PerfilPageComponent } from './features/profile/perfil-page/perfil-page.component';
 import { authGuard, publicGuard } from './core/guards/auth.guard';
 import { adminGuard } from './core/guards/admin.guard';
-import { BookingWrapperComponent } from './features/bookings/booking-wrapper/booking-wrapper.component';
+import { BookingPageComponent } from './features/bookings/booking-page/booking-page.component';
 import { AppointmentsPageComponent } from './features/appointments/appointments-page/appointments-page.component';
 import { AppointmentDetailPageComponent } from './features/appointments/appointment-detail-page/appointment-detail-page.component';
 import { ServicesPageComponent } from './features/services/services-page/services-page.component';
@@ -38,7 +38,7 @@ export const routes: Routes = [
   },
   {
     path: 'booking',
-    component: BookingWrapperComponent,
+    component: BookingPageComponent,
     data: { viewTransitionName: 'booking' },
   },
   {
