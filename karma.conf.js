1e0b901f53460c51fa61cd980d109f54aa0fa2b6
arnaum13 <arnaumm98@gmail.com>
2025-08-26 16:52:28 +0200

    fix karma + coverage

--- karma.conf.js
+++ karma.conf.js
@@ -42,6 +42,8 @@ module.exports = function (config) {
           '--no-sandbox',
           '--disable-gpu',
           '--disable-dev-shm-usage',
+          '--disable-web-security',
+          '--disable-features=VizDisplayCompositor',
         ],
       },
     },
